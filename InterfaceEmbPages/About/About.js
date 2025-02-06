0c9874af2128d1432b855d9911b5787ef0b49477
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2025-02-06 21:15:22 +0100

    Add files via upload

---
+++ InterfaceEmbPages/About/About.js
@@ -0,0 +1,5 @@
+const read_report = document.querySelector('.read-report');
+
+read_report.onclick = function() {
+    window.open('https://drive.google.com/file/d/1difmZx4Rn27IOeYc2dmJQaQEHMvz_XT2/view?usp=drive_link', '_blank');
+}
