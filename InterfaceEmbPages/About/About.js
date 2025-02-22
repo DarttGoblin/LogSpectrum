0128d76ced28008bd28d79d1f75039a1cc79ca6e
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2025-02-22 23:35:04 +0100

    Update About.js

--- InterfaceEmbPages/About/About.js
+++ InterfaceEmbPages/About/About.js
@@ -1,4 +1,9 @@
 const read_report = document.querySelector('.read-report');
+const professor = document.querySelector('.professor');
+
+professor.onclick = function() {
+    window.open('https://www.linkedin.com/in/mariyaouaissa/', '_blank');
+}
 
 read_report.onclick = function() {
     window.open('https://drive.google.com/file/d/1difmZx4Rn27IOeYc2dmJQaQEHMvz_XT2/view?usp=drive_link', '_blank');
